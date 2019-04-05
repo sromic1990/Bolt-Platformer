64691dd04cb3a2b90981c839cc541209e73afc8a
Sourav Chatterjee <sromic@gmail.com>
2019-04-05 19:01:59 +0530

    [Update] Character controller established for platformer character.

--- Assets/AiUnity/UserData/MultipleTags/TagAccess.cs
+++ Assets/AiUnity/UserData/MultipleTags/TagAccess.cs
@@ -5,7 +5,7 @@ using AiUnity.Common.Tags;
 /// <Summary>
 /// Provide strongly typed access to Unity tags.
 /// <Summary>
-[GeneratedType("04/05/2019 16:05:36")]
+[GeneratedType("04/05/2019 16:26:20")]
 public class TagAccess : ITagAccess
 {
 	public const string Untagged = "Untagged";
@@ -15,6 +15,10 @@ public class TagAccess : ITagAccess
 	public const string MainCamera = "MainCamera";
 	public const string Player = "Player";
 	public const string GameController = "GameController";
+	public const string Platform = "Platform";
+	public const string Projectile = "Projectile";
+	public const string Key = "Key";
+	public const string Enemy = "Enemy";
 
 	private static readonly List<string> tagPaths = new List<string>()
 	{
@@ -24,7 +28,11 @@ public class TagAccess : ITagAccess
 		"EditorOnly",
 		"MainCamera",
 		"Player",
-		"GameController"
+		"GameController",
+		"Platform",
+		"Projectile",
+		"Key",
+		"Enemy"
 	};
 
 	public IEnumerable<string> TagPaths { get { return tagPaths.AsReadOnly(); } }
